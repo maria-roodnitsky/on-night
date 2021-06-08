193cd6d5973def79b46465562de010c8f7e67a10
william-toth <wat4th@gmail.com>
2021-06-08 11:17:58 -0700

    calendar fun

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, TouchableOpacity, Text, SafeAreaView, ImageBackground, Dimensions} from 'react-native';
+import {View, TouchableOpacity, Text, SafeAreaView, ImageBackground, Image, Dimensions} from 'react-native';
 import {Agenda} from 'react-native-calendars';
 import {Card, Avatar} from 'react-native-paper';
 import axios from 'axios';
@@ -11,7 +11,11 @@ const timeToString = (time) => {
   return date.toISOString().split('T')[0];
 };
 
-const Schedule: React.FC = (props) => {
+interface ScheduleProps {
+  token: string,
+}
+
+const Schedule: React.FC<ScheduleProps> = (props) => {
   const [items, setItems] = useState({});
   const [events, setEvents] = useState({});
 
@@ -68,6 +72,58 @@ const Schedule: React.FC = (props) => {
     })
   }
 
+  const renderImage = (event) => {
+    let imgSource = require('../img/greek_spaces/none.png');
+        if (event.location == 'Alpha Chi') {
+            imgSource = require('../img/greek_spaces/alphachi.jpg');
+        } else if (event.location == 'Alpha Theta') {
+            imgSource = require('../img/greek_spaces/alphatheta.jpg');
+        } else if (event.location == 'Alpha Phi') {
+            imgSource = require('../img/greek_spaces/aphi.jpg');
+        } else if (event.location == 'Alpha Xi Delta') {
+            imgSource = require('../img/greek_spaces/axid.jpg');
+        } else if (event.location == 'Beta Alpha Omega') {
+            imgSource = require('../img/greek_spaces/beta.jpg');
+        } else if (event.location == 'Bones Gate') {
+            imgSource = require('../img/greek_spaces/bg.jpg');
+        } else if (event.location == 'Chi Delta') {
+            imgSource = require('../img/greek_spaces/chidelt.jpg');
+        } else if (event.location == 'Chi Gamma Epsilon') {
+            imgSource = require('../img/greek_spaces/chigam.jpg');
+        } else if (event.location == 'Epsilon Kappa Theta') {
+            imgSource = require('../img/greek_spaces/ekt.jpg');
+        } else if (event.location == 'Gamma Delta Chi') {
+            imgSource = require('../img/greek_spaces/gdx.jpg');
+        } else if (event.location == 'Chi Heorot') {
+            imgSource = require('../img/greek_spaces/heorot.jpg');
+        } else if (event.location == 'Kappa Kappa Gamma') {
+            imgSource = require('../img/greek_spaces/kappa.jpg');
+        } else if (event.location == 'Kappa Delta') {
+            imgSource = require('../img/greek_spaces/kd.jpg');
+        } else if (event.location == 'Kappa Delta Epsilon') {
+            imgSource = require('../img/greek_spaces/kde.jpg');
+        } else if (event.location == 'Phi Delta') {
+            imgSource = require('../img/greek_spaces/phidelt.jpg');
+        } else if (event.location == 'Phi Tau') {
+            imgSource = require('../img/greek_spaces/phitau.jpg');
+        } else if (event.location == 'Psi Upsilon') {
+            imgSource=require('../img/greek_spaces/psiu.jpg');
+        } else if (event.location == 'Sigma Alpha Epsilon') {
+            imgSource = require('../img/greek_spaces/sae.jpg');
+        } else if (event.location == 'Sigma Delta') {
+            imgSource = require('../img/greek_spaces/sigmadelt.jpg');
+        } else if (event.location == 'Sigma Nu') {
+            imgSource = require('../img/greek_spaces/signu.jpg');
+        } else if (event.location == 'Tabard') {
+            imgSource = require('../img/greek_spaces/tabard.jpg');
+        } else if (event.location == 'Theta Delta Chi') {
+            imgSource = require('../img/greek_spaces/tdx.jpg');
+        } else if (event.location == 'Tri Kap') {
+            imgSource = require('../img/greek_spaces/trikap.jpg');
+        }
+        return <Image source={imgSource} style={{width: 75, height: 75, borderRadius: 50}}/>;
+    }
+
   const renderItem = (item) => {
     return (
       <TouchableOpacity style={{marginRight: 10, marginTop: 17, marginBottom:50}}>
@@ -98,7 +154,7 @@ const Schedule: React.FC = (props) => {
                 flexDirection: 'column',
                 justifyContent: 'space-between',
               }}>
-              <Avatar.Text label="E" />
+              {renderImage(event)}
               <Text style={{margin: 3, fontFamily: 'Open-Sans', fontSize: 20}}>{event.title}</Text>
               <Text style={{margin: 3, fontFamily: 'Open-Sans'}}>{event.location}, {event.time}</Text>
               <Text style={{margin: 3, fontFamily: 'Open-Sans'}}>{event.description}</Text>
